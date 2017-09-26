8e7667d727f20dd08ba53416bc10602035251e5a
Adam He <shuaihe2018@u.northwestern.edu>
2017-09-26 01:23:47 -0500

    directions functionality with alarm

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -15,10 +15,8 @@ declare var google;
 })
 export class HomePage {
 
-  @ViewChild('map') mapElement: ElementRef;
-  map: any;
-  start = 'chicago, il';
-  end = 'chicago, il';
+  departureAddress: string;
+  arrivalAddress: string;
   directionsService = new google.maps.DirectionsService;
   directionsDisplay = new google.maps.DirectionsRenderer;
 
@@ -29,7 +27,6 @@ export class HomePage {
 
   constructor(public platform: Platform, public navCtrl: NavController, private localNotifications: LocalNotifications, private media: Media) {
     // timezone conversion
-    this.departureTime = (new Date(new Date().getTime() - new Date().getTimezoneOffset()*60000)).toISOString();
     this.platform.ready().then((readysource) => {
       if (this.platform.is('android')) {
         this.alarmFile = this.media.create('file:///android_asset/www/sounds/alarm.mp3');
@@ -37,16 +34,23 @@ export class HomePage {
         this.alarmFile = this.media.create('/var/mobile/Applications/<UUID>/alarm.mp3');
       }
 
+      this.arrivalTime = (new Date(new Date().getTime() - new Date().getTimezoneOffset()*60000)).toISOString();
+      this.departureTime = (new Date(new Date().getTime() - new Date().getTimezoneOffset()*60000)).toISOString();
+
       this.localNotifications.on('trigger', () => this.alarmFile.play());
       this.localNotifications.on('clear', () => this.alarmFile.stop());
     });
   }
 
+  ConvertTimeZone(time: Date) {
+    var resultTime = new Date(time);
+    resultTime.setHours(resultTime.getHours() + resultTime.getTimezoneOffset() / 60);
+    resultTime.setSeconds(0);
+    return resultTime;
+  }
+
   SetAlarm(time: Date) {
-    // timezone conversion
-    var alarmTime = new Date(time);
-    alarmTime.setHours(alarmTime.getHours() + alarmTime.getTimezoneOffset() / 60);
-    alarmTime.setSeconds(0);
+    var alarmTime = this.ConvertTimeZone(time);
 
     this.localNotifications.schedule({
       id: 0,
@@ -58,27 +62,19 @@ export class HomePage {
     console.log("Alarm set at ", alarmTime.toString());
   }
 
-  ionViewDidLoad(){
-    this.initMap();
-  }
-
-  initMap() {
-    this.map = new google.maps.Map(this.mapElement.nativeElement, {
-      zoom: 7,
-      center: {lat: 41.85, lng: -87.65}
-    });
-
-    this.directionsDisplay.setMap(this.map);
-  }
-
-  calculateAndDisplayRoute() {
+  CalculateRoute() {
     this.directionsService.route({
-      origin: this.start,
-      destination: this.end,
-      travelMode: 'DRIVING'
+      origin: this.departureAddress,
+      destination: this.arrivalAddress,
+      travelMode: 'TRANSIT',
+      transitOptions: {
+        arrivalTime: this.ConvertTimeZone(new Date(this.arrivalTime))
+      }
     }, (response, status) => {
       if (status === 'OK') {
-        this.directionsDisplay.setDirections(response);
+        var duration = response.routes[0].legs[0].duration.value * 1000;
+        this.departureTime = new Date(new Date(this.arrivalTime).getTime() - duration).toISOString();
+        this.SetAlarm(new Date(this.departureTime));
       } else {
         window.alert('Directions request failed due to ' + status);
       }
