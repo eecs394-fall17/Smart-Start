2eecfac25ad6ef4ca7534f3345552d2a8361350b
brotatotes <erichao2018@u.northwestern.edu>
2017-10-15 13:10:29 -0500

    Feature - Able to edit alarms

--- src/components/alarm-card/alarm-card.ts
+++ src/components/alarm-card/alarm-card.ts
@@ -1,4 +1,7 @@
 import { Component, Input, Output } from '@angular/core';
+import { HomePage } from '../../pages/home/home';
+import { AlarmDisplayPage } from '../../pages/alarm-display/alarm-display';
+import { ModalController } from 'ionic-angular';
 
 /**
  * Generated class for the AlarmCardComponent component.
@@ -12,31 +15,69 @@ import { Component, Input, Output } from '@angular/core';
 })
 export class AlarmCardComponent {
 
-	@Input('alarmTime') alarmTime: Date;
-	@Input('departureTime') departureTime: Date;
+  @Input('alarmTime') alarmTime: Date;
+  @Input('departureTime') departureTime: Date;
   @Input('arrivalTime') arrivalTime: Date;
   @Input('destination') destination: string;
   @Input('tripDurationString') tripDurationString: string;
   @Input('readyTimeString') readyTimeString: string;
 
+  // not displayed
+  @Input('alarmId') alarmId: number;
+  @Input('readyTime') readyTime: number;
+  @Input('departureAddress') departureAddress: string;
+
   alarmExpand: boolean;
 
   toggleAlarm = function() {
-  	this.alarmExpand = !this.alarmExpand;
-  	console.log(this.alarmExpand);
+    this.alarmExpand = !this.alarmExpand;
+    console.log(this.alarmExpand);
   };
 
   isAlarmExpanded = function() {
     return this.alarmExpand;
   };
 
-  constructor() {
-  	this.alarmExpand = false;
-  	// this.departureTime = departureTime;
-  	// this.arrivalTime = arrivalTime;
-  	// this.destination = destination;
-  	// this.tripDurationString = tripDurationString;
-  	// this.readyTimeString = readyTimeString;
+  constructor(
+    public modalController: ModalController
+  ) {
+    this.alarmExpand = false;
+  }
+
+  PresentEditAlarmModal() {
+    console.log(this.departureAddress, this.readyTime);
+    let modal = this.modalController.create(HomePage, {
+      purpose: 'Edit',
+      arrivalTime: this.arrivalTime,
+      destination: this.destination,
+      departureAddress: this.departureAddress,
+      readyTime: this.readyTime
+    });
+
+    modal.onDidDismiss(data => {
+      if (data != null) {
+        this.departureTime = data.departureTime;
+        this.arrivalTime = data.arrivalTime;
+        this.destination = data.destination;
+        this.tripDurationString = this.DurationToString(data.tripDuration)
+        this.readyTimeString = this.DurationToString(data.readyTime * 60000);
+        this.readyTime = data.readyTime;
+        this.alarmTime = data.alarmTime;
+      }
+    });
+
+    modal.present()
+  }
+
+  DurationToString(time: number) {
+    var result = '';
+    time = Math.floor(time / 60000);
+    if (time < 60) {
+      result = time.toString() + " minutes";
+    } else {
+      result = (Math.floor(time / 60)).toString() + " hours and " + (time % 60).toString() + " minutes";
+    }
+    return result
   }
 
 }
